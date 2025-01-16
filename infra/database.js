b5bfe6c6dfd10c688235b486cecb759c579c3885
UmbreonX2 <matheuslivio2328@gmail.com>
2025-01-15 21:47:11 -0300

    make database.js more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -1,3 +1,31 @@
+// import { Pool } from 'pg';
+
+
+// const pool = new Pool({
+//   host: process.env.POSTGRES_HOST,
+//   port: process.env.POSTGRES_PORT,
+//   user: process.env.POSTGRES_USER,
+//   database: process.env.POSTGRES_DB,
+//   password: process.env.POSTGRES_PASSWORD
+// });
+
+// async function query(queryObject) {
+//   const client = await pool.connect();
+
+//   try {
+//     const result = await pool.query(queryObject);
+//     return result;
+//   } catch (err) {
+//     console.error(err);
+//   } finally {
+//     client.release();
+//   }
+// }
+
+// export default {
+//   query: query,
+// };
+
 import { Client } from 'pg';
 
 async function query(queryObject) {
@@ -10,9 +38,14 @@ async function query(queryObject) {
   });
 
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
